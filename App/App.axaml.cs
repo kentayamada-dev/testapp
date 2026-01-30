407a70953d9e65b13f7161249159eb6719518775
Kenta Yamada <kentayamada058@gmail.com>
2026-01-30 00:25:20 -0500

    test doc

--- App/App.axaml.cs
+++ App/App.axaml.cs
@@ -77,6 +77,17 @@ public sealed class App : Application
       desktop.ShutdownMode = shutdownMode;
       desktop.MainWindow = _mainWindow;
 
+      if (OperatingSystem.IsMacOS())
+      {
+        desktop.Startup += (_, _) =>
+        {
+          if (_mainWindow.IsVisible) return;
+
+          _mainWindow.Show();
+          _mainWindow.Activate();
+        };
+      }
+
       if (OperatingSystem.IsWindows())
       {
         _singleInstanceService.StartActivateListener(_mainWindow);
